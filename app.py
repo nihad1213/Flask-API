9cd5fd44a1ebc70d59f9f37e62125984a4cc14ee
nihad1213 <nihad.nemetli@gmail.com>
2024-07-01 19:16:33 +0400

    App.py created

---
+++ app.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+"""Importing Libraries and Modules"""
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+# Creating Flask App
+app = Flask(__name__)
+
+# Configuration for MySQL
+DB_URI = "mysql://root:''@localhost/hbtn" # dbusername:'dbpassword'@servername/dbname
+app.config['SQLALCHEMY_DATABASE_URI'] = DB_URI 
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # Improve performace. Doesnt show warnings. Using: (Optinal)
+
+# Setting Up Database 
+dataBase = SQLAlchemy(app)
+
+# Basic Route send us to welcome page
+@app.route('/')
+def welcome():
+    return "Sagopa Kajmer"
+
+if __name__ == "__main__":
+    # Used to create an application context
+    with app.app_context():
+        # Create all tables if they do not exist
+        dataBase.create_all()
+    
+    # Run the Flask application
+    app.run(debug=True)
